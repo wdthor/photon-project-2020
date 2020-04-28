85abd9a4daa8d55b7df0941433a34896d54c3f9a
webdevthierry <thierry.hor.sd@gmail.com>
2020-04-28 02:02:15 +0200

    Get photos from Pexels API

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// 563492ad6f917000010000019de2bef887934a00a8ffb077fe87f3fb
+const auth = "563492ad6f917000010000019de2bef887934a00a8ffb077fe87f3fb";
+const gallery = document.querySelector(".gallery");
+const searchInput = document.querySelector(".search-input");
+const submitButton = document.querySelector(".submit-btn");
+let searchValue;
+
+async function curatedPhotos() {
+    const dataFetch = await fetch(
+        "https://api.pexels.com/v1/curated?per_page=15&page=1",
+        {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: auth,
+            },
+        }
+    );
+    const data = await dataFetch.json();
+    data.photos.forEach((photo) => {
+        const galleryImg = document.createElement("div");
+        galleryImg.classList.add("gallery-img");
+        galleryImg.innerHTML = `<img src="${photo.src.large}"></img>
+        <p>${photo.photographer}</p>`;
+        gallery.appendChild(galleryImg);
+    });
+}
+
+curatedPhotos();
