adc8e88669f676c1ef290646eccd455394aaad7b
webdevthierry <thierry.hor.sd@gmail.com>
2020-04-30 01:34:10 +0200

    Refactor API calls, and photos generated

--- app.js
+++ app.js
@@ -17,18 +17,19 @@ function updateInput(e) {
     searchValue = e.target.value;
 }
 
-async function curatedPhotos() {
-    const dataFetch = await fetch(
-        "https://api.pexels.com/v1/curated?per_page=15&page=1",
-        {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                Authorization: auth,
-            },
-        }
-    );
+async function fetchApi(url) {
+    const dataFetch = await fetch(url, {
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            Authorization: auth,
+        },
+    });
     const data = await dataFetch.json();
+    return data;
+}
+
+function generatePicture(data) {
     data.photos.forEach((photo) => {
         const galleryImg = document.createElement("div");
         galleryImg.classList.add("gallery-img");
@@ -38,25 +39,20 @@ async function curatedPhotos() {
     });
 }
 
-async function searchPhotos(query) {
-    const dataFetch = await fetch(
-        `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`,
-        {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                Authorization: auth,
-            },
-        }
+async function curatedPhotos() {
+    const data = await fetchApi(
+        "https://api.pexels.com/v1/curated?per_page=15&page=1"
     );
-    const data = await dataFetch.json();
-    data.photos.forEach((photo) => {
-        const galleryImg = document.createElement("div");
-        galleryImg.classList.add("gallery-img");
-        galleryImg.innerHTML = `<img src="${photo.src.large}"></img>
-        <p>${photo.photographer}</p>`;
-        gallery.appendChild(galleryImg);
-    });
+
+    generatePicture(data);
+}
+
+async function searchPhotos(query) {
+    const data = await fetchApi(
+        `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
+    );
+
+    generatePicture(data);
 }
 
 curatedPhotos();
