3e7f61ac276cf394f46e16386f54b880d0a9483f
webdevthierry <thierry.hor.sd@gmail.com>
2020-04-30 01:45:18 +0200

    clear gallery and search input, when searching, add photo download link

--- app.js
+++ app.js
@@ -33,8 +33,13 @@ function generatePicture(data) {
     data.photos.forEach((photo) => {
         const galleryImg = document.createElement("div");
         galleryImg.classList.add("gallery-img");
-        galleryImg.innerHTML = `<img src="${photo.src.large}"></img>
-        <p>${photo.photographer}</p>`;
+        galleryImg.innerHTML = `
+        <div class="gallery-info">
+            <p>${photo.photographer}</p>
+            <a href=${photo.src.original}>Download</a>
+        </div>
+        <img src="${photo.src.large}"></img>
+        `;
         gallery.appendChild(galleryImg);
     });
 }
@@ -48,11 +53,17 @@ async function curatedPhotos() {
 }
 
 async function searchPhotos(query) {
+    clear();
     const data = await fetchApi(
         `https://api.pexels.com/v1/search?query=${query}+query&per_page=15&page=1`
     );
 
     generatePicture(data);
+}
+
+function clear() {
+    gallery.innerHTML = "";
+    searchInput.value = "";
 }
 
 curatedPhotos();
